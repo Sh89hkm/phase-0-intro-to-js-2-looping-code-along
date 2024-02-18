a8868e77a28519e63efc449eb71721a11eabcf81
shvan <shvanabdulhakeem2020@gmail.com>
2024-02-18 20:46:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+function writeCards(guests, party) {
+    const messages = [];
+    for (let i = 0; i < guests.length; i++) {
+        messages.push(`Thank you, ${guests[i]}, for the wonderful ${party} gift!`);
+    }
+    return messages;
+}
+
+const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(messages);
+
+function countDown(startingNumber) {
+    if (startingNumber < 0) {
+        console.log("Please provide a positive integer.");
+        return;
+    }
+
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
